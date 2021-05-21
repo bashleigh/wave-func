7e68c9a401bfc013d783f5e3daf38fd336932dcf
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2021-05-21 17:23:36 +0100

    feat: added elements

---
+++ src/elements/index.ts
@@ -0,0 +1,494 @@
+import { Neutron, Proton } from "../hardons";
+import { Hardon } from "../types";
+
+export enum ElementName {
+  HYDROGEN = 'hydrogen',
+  HELIUM = 'helium',
+  LITHIUM = 'lithium',
+  BERYLLIUM = 'beryllium',
+  BORON = 'boron',
+  CARBON = 'carbon',
+  NITROGEN = 'nitrogen',
+  OXYGEN = 'oxygen',
+  FLUORINE = 'fluorine',
+  NEON = 'neon',
+  SODIUM = 'sodium',
+  MAGNESIUM = 'magnesium',
+  ALUMINIUM = 'aluminium',
+  SILICON = 'silicon',
+  PHOSPHORUS = 'phosphorus',
+  SULFUR = 'sulfur',
+  CHLORINE = 'chlorine',
+  ARGON = 'argon',
+  POTASSIUM = 'potassium',
+  CALCIUM = 'calcium',
+  SCANDIUM = 'scandium',
+  TITANIUM = 'titanium',
+  VANADIUM = 'vanadium',
+  CHROMIUM = 'chromium',
+  MANGANESE = 'manganese',
+  IRON = 'iron',
+  COLBOLT = 'colbolt',
+  NICKEL = 'nickel',
+  COPPER = 'copper',
+  ZINC = 'zinc',
+  GALLIUM = 'gallium',
+  GERMANIUM = 'germanium',
+  ARSENIC = 'arsenic',
+  SELENIUM = 'selenium',
+  BORMINE = 'bormine',
+  KRYPTON = 'krypton',
+  RUBIDIUM = 'rubidium',
+  STRONTIUM = 'strontium',
+  YTTRIUM = 'yttrium',
+  ZIRCONIUM = 'zirconium',
+  NIOBIUM = 'niobium',
+  MOLYBDENUM = 'molbdenum',
+  TECHNETIUM = 'technetium',
+  RUTHENIUM = 'ruthenium',
+  RHODIUM = 'rhodium',
+  PALLADIUM = 'palladium',
+  SILVER = 'silver',
+  CADMIUM = 'cadmium',
+  INDIUM = 'indium',
+  TIN = 'tin',
+  ANTIMONY = 'antimony',
+  TELLURIUM = 'tellurium',
+  IODINE = 'iodine',
+  XNEON = 'xneon',
+  CASIUM = 'casium',
+  BARIUM = 'barium',
+  LUTETIUM = 'lutetium',
+  HAFNIUM = 'hafnium',
+  TANTALUM = 'tantalum',
+  TUNGSTEN = 'tungsten',
+  RHENIUM = 'rhenium',
+  OSMIUM = 'osmium',
+  IRIDIUM = 'iridium',
+  PLATINUM = 'pltinum',
+  GOLD = 'gold',
+  MERCURY = 'mercury',
+  THALLIUM = 'thallium',
+  LEAD = 'lead',
+  BISMUTH = 'bismuth',
+  POLONIUM = 'polonium',
+  ASTATINE = 'astatine',
+  RADON = 'radon',
+  FRANCIUM = 'francium',
+  RADIUM = 'radium',
+  LAWRENCIUM = 'lawrencium',
+  RUTHERFORDIUM = 'rutherfordium',
+  DUBNIUM = 'dubnium',
+  SEABORGIUM = 'seaborgium',
+  BOHRIUM = 'bohrium',
+  HASSIUM = 'hassium',
+  MEITNERIUM = 'meitnerium',
+  DARMSTADTIUM = 'darmstadtium',
+  REONTGENIUM = 'reontgenium',
+  COPERNICIUM = 'copernicium',
+  NIHONIUM = 'nihonium',
+  FLEROVIUM = 'flerovium',
+  MOSCOVIUM = 'moscovium',
+  LIVERMORIUM = 'livermorium',
+  TENNESSINE = 'tennessine',
+  OGANESSON = 'oganesson',
+
+}
+
+export enum ElementaryGroup {
+  HYDROGEN = 'hydrogen',
+  NOBLE_GAS = 'noble gas',
+  ALKALIN_METAL = 'alkalin metal',
+  BORON_GROUP = 'boron group',
+  CARBON_GROUP = 'carbon group',
+  PNICTOGENS_GROUP = 'pinctogens group',
+  CHALCOGENS_GROUP = 'chalcogens group',
+  HALOGEN = 'halogen group',
+}
+
+export interface Element {
+  hardons: Hardon[];
+  electrons: number[];
+  name: ElementName;
+  symbol: string;
+  meltingPoint?: number;
+  bolingPoint?: number;
+  group: ElementaryGroup;
+  number: number;
+}
+
+export class Hydrogen implements Element {
+  name = ElementName.HYDROGEN;
+  number = 1;
+  symbol = 'H';
+  hardons = [
+    new Proton(),
+  ];
+  electrons = [1];
+  group = ElementaryGroup.HYDROGEN;
+  meltingPoint = 13.99;
+  bolingPoint = 20.271;
+}
+
+export class Helium implements Element {
+  name = ElementName.HELIUM;
+  number = 2;
+  symbol = 'He';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2];
+  group = ElementaryGroup.NOBLE_GAS;
+  meltingPoint = .95;
+  bolingPoint = 4.222;
+}
+
+export class Lithium implements Element {
+  name = ElementName.LITHIUM;
+  number = 3;
+  symbol = 'Li';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 1];
+  group = ElementaryGroup.ALKALIN_METAL;
+  meltingPoint = 453.65;
+  bolingPoint = 1603;
+}
+
+export class Beryllium implements Element {
+  name = ElementName.BERYLLIUM;
+  number = 4;
+  symbol = 'Be';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 2];
+  group = ElementaryGroup.ALKALIN_METAL;
+  meltingPoint = 1560;
+  bolingPoint = 2742;
+}
+
+export class Boron implements Element {
+  name = ElementName.BORON;
+  number = 5;
+  symbol = 'B';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 3];
+  group = ElementaryGroup.BORON_GROUP;
+  meltingPoint = 2349;
+  bolingPoint = 4200;
+}
+
+export class Carbon implements Element {
+  name = ElementName.CARBON;
+  number = 6;
+  symbol = 'C';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 4];
+  group = ElementaryGroup.CARBON_GROUP;
+}
+
+export class Nitrogen implements Element {
+  name = ElementName.NITROGEN;
+  number = 7;
+  symbol = 'N';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+  ];
+  electrons = [2, 5];
+  group = ElementaryGroup.PNICTOGENS_GROUP;
+  bolingPoint = 77.355;
+  meltingPoint = 63.23;
+}
+
+export class Oxygen implements Element {
+  name = ElementName.OXYGEN;
+  number = 8;
+  symbol = 'O';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 6];
+  group = ElementaryGroup.CHALCOGENS_GROUP;
+  meltingPoint = 54.36;
+  bolingPoint = 90.188;
+}
+
+export class Fluorine implements Element {
+  name = ElementName.FLUORINE;
+  number = 9;
+  symbol = 'F';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 7];
+  group = ElementaryGroup.HALOGEN;
+  meltingPoint = 53.48;
+  bolingPoint = 85.03;
+}
+
+export class Neon implements Element {
+  name = ElementName.NEON;
+  number = 10;
+  symbol = 'Ne';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 8];
+  group = ElementaryGroup.NOBLE_GAS;
+  meltingPoint = 24.56;
+  bolingPoint = 27.104;
+}
+
+export class Sodium implements Element {
+  name = ElementName.SODIUM;
+  number = 11;
+  symbol = 'Na';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 8, 1];
+  group = ElementaryGroup.ALKALIN_METAL;
+  meltingPoint = 370.944;
+  bolingPoint = 1156.09;
+}
+
+export class Magnesium implements Element {
+  name = ElementName.MAGNESIUM;
+  number = 12;
+  symbol = 'Mg';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 8, 2];
+  group = ElementaryGroup.ALKALIN_METAL;
+  meltingPoint = 923;
+  bolingPoint = 1363;
+}
+
+export class Aluminium implements Element {
+  name = ElementName.ALUMINIUM;
+  number = 13;
+  symbol = 'Al';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 8, 3];
+  group = ElementaryGroup.BORON_GROUP;
+  meltingPoint = 933.47;
+  bolingPoint = 2743;
+}
+
+
+export class Silicon implements Element {
+  name = ElementName.SILICON;
+  number = 14;
+  symbol = 'Si';
+  hardons = [
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),    
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Proton(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),    
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+    new Neutron(),
+  ];
+  electrons = [2, 8, 4];
+  group = ElementaryGroup.CARBON_GROUP;
+  meltingPoint = 1687;
+  bolingPoint = 3538;
+}
